8ee435070aaa9888c5d86eedacbbdd7a80043c2a
J Hershey <hersheyj32@gmail.com>
2019-03-23 20:43:54 -0700

    let this be the starting

--- JENPY/Program.cs
+++ JENPY/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -6,161 +7,87 @@ using System.Threading;
 
 namespace JENPY
 {
-    class Program
+    class TcpServer
     {
-        public static ManualResetEvent allDone = new ManualResetEvent(false);
+        private TcpListener _server;
+        private Boolean _isRunning;
 
-        public class StateObject {
-            public Socket workSocket = null;
-
-            public const int BufferSize = 1024;
-
-            public byte[] buffer = new byte[BufferSize];
-
-            public StringBuilder sb = new StringBuilder();
-
-        }
-        public const int Port = 5026;
-        static void Main(string[] args)
+        public TcpServer(int port)
         {
-            Console.WriteLine("Hello World!");
-            StartListening();
+            _server = new TcpListener(IPAddress.Any, port);
+            _server.Start();
 
+            _isRunning = true;
+
+            LoopClients();
         }
 
-        private static void StartListening()
+        public void LoopClients()
         {
+            while (_isRunning)
+            {
+                // wait for client connection
+                TcpClient newClient = _server.AcceptTcpClient();
 
-            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
-            IPAddress ipAddress = ipHostInfo.AddressList[0];
-            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, Port);
+                // client found.
+                // create a thread to handle communication
+                Thread t = new Thread(new ParameterizedThreadStart(HandleClient));
+                t.Start(newClient);
+            }
+        }
 
-            //creating the socket...
-            Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+        public void HandleClient(object obj)
+        {
+            // retrieve client from parameter passed to thread
+            TcpClient client = (TcpClient)obj;
 
-            //bind the socket
+            // sets two streams
+            StreamWriter sWriter = new StreamWriter(client.GetStream(), Encoding.ASCII);
+            StreamReader sReader = new StreamReader(client.GetStream(), Encoding.ASCII);
+            // you could use the NetworkStream to read and write, 
+            // but there is no forcing flush, even when requested
+
+            Boolean bClientConnected = true;
+            String sData = null;
             try
             {
-                listener.Bind(localEndPoint);
-                listener.Listen(100);
-
-                while (true) {
-                    allDone.Reset(); // TODO, learn MORE
-
-                    Console.WriteLine("waiting for a connection on: " + ipAddress + " port " + Port);
-                    listener.BeginAccept(new AsyncCallback(AcceptCallback), listener);
-                    allDone.WaitOne();
-                }
-            }
-            catch(Exception e) {
-                Console.WriteLine("oh e happened eh" + e.StackTrace);
-            }
-
-            Console.WriteLine("press ENTER to continue");
-            Console.Read();
-
-        }
-
-        private static void AcceptCallback(IAsyncResult ar)
-        {
-            Console.WriteLine("calling accept callback");
-            allDone.Set(); // signal to main thread to continue
-
-            Socket listener = (Socket)ar.AsyncState;
-            Socket handler = listener.EndAccept(ar);
-
-            StateObject state = new StateObject();
-            state.workSocket = handler;
-
-            handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0, new AsyncCallback(ReadCallback), state); 
-
-        }
-
-        private static void ReadCallback(IAsyncResult ar)
-        {
-            String content = String.Empty;
-
-            // Retrieve the state object and the handler socket  
-            // from the asynchronous state object.  
-            StateObject state = (StateObject) ar.AsyncState;
-            Socket handler = state.workSocket;
-
-            // Read data from the client socket.   
-            int bytesRead = handler.EndReceive(ar);
-
-            if (bytesRead > 0)
-            {
-                // There  might be more data, so store the data received so far.  
-                state.sb.Append(Encoding.ASCII.GetString(
-                    state.buffer, 0, bytesRead));
-
-                // Check for end-of-file tag. If it is not there, read   
-                // more data.  
-                content = state.sb.ToString();
-                if (content.IndexOf("<EOF>", StringComparison.Ordinal) > -1)
+                while (bClientConnected)
                 {
-                    // All the data has been read from the   
-                    // client. Display it on the console.  
-                    Console.WriteLine("Read {0} bytes from socket. \n Data : {1}",
-                        content.Length, content);
-                    // Echo the data back to the client.  
-                    Send(handler, content);
+
+                    // reads from stream
+                    sData = sReader.ReadLine();
+
+                    if (sData == "EXIT") {
+                        sWriter.Write("Exiting\n");
+                        sWriter.Flush();
+                        sWriter.Close();
+                        sReader.Close();
+                        return;
+                    }
+
+                    // shows content on the console.
+                    Console.WriteLine("Client &gt; " + sData);
+
+                    // to write something back.
+                    sWriter.WriteLine("Meaningfull things here");
+                    sWriter.Flush();
                 }
-                else
-                {
-                    // Not all data received. Get more.  
-                    //handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
-                    new AsyncCallback(JenpyReaderCallBack), state);
-                }
-            }
-        }
-
-        private static void JenpyReaderCallBack(IAsyncResult ar) {
-            string content = "hi world, make Jenpy response";
-
-
-            // Retrieve the state object and the handler socket  
-            // from the asynchronous state object.  
-            StateObject state = (StateObject)ar.AsyncState;
-            Socket handler = state.workSocket;
-
-            Send(handler, content);
-
-        }
-
-        private static void Send(Socket handler, string data)
-        {
-            // Convert the string data to byte data using ASCII encoding.  
-            byte[] byteData = Encoding.ASCII.GetBytes(data);
-
-            // Begin sending the data to the remote device.  
-            handler.BeginSend(byteData, 0, byteData.Length, 0,
-                new AsyncCallback(SendCallback), handler);
-        }
-
-        private static void SendCallback(IAsyncResult ar)
-        {
-            try
-            {
-                // Retrieve the socket from the state object.  
-                Socket handler = (Socket)ar.AsyncState;
-
-                // Complete sending the data to the remote device.  
-                int bytesSent = handler.EndSend(ar);
-                Console.WriteLine("Sent {0} bytes to client.", bytesSent);
-
-                handler.Shutdown(SocketShutdown.Both);
-                handler.Close();
-
             }
             catch (Exception e)
             {
-                Console.WriteLine(e.ToString());
+
+                Console.WriteLine("caught exception");
+                Console.WriteLine(e.StackTrace);
             }
         }
+    }
 
-
-
-
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Multi-Threaded TCP Server Demo");
+            TcpServer server = new TcpServer(5555);
+        }
     }
 }
---
+++ JENPY/handleClient.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace JENPY
+{
+public class handleClient
+    {
+        TcpClient clientSocket;
+
+        public void startClient(TcpClient inClientSocket)
+        {
+            this.clientSocket = inClientSocket;
+            Thread ctThread = new Thread(Chat);
+            ctThread.Start();
+        }
+
+        private void Chat()
+        {
+            StreamReader reader = new StreamReader(clientSocket.GetStream());
+
+            try
+            {
+                while (true)
+                {
+                    string message = reader.ReadLine();
+                    Console.WriteLine("received message" + message);
+                    //foreach (var client in Program.GetClients())
+                    //{
+                    //    StreamWriter writer = new StreamWriter(client.GetStream());
+                    //    writer.Write(message);
+                    //}
+                    StreamWriter writer = new StreamWriter(clientSocket.GetStream());
+                    writer.Write(message);
+                }
+            }
+            catch (EndOfStreamException)
+            {
+                Console.WriteLine($"client disconnecting: {clientSocket.Client.RemoteEndPoint}");
+                clientSocket.Client.Shutdown(SocketShutdown.Both);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"IOException reading from {clientSocket.Client.RemoteEndPoint}: {e.Message}");
+            }
+
+            clientSocket.Close();
+            Program.RemoveClient(clientSocket);
+            Console.WriteLine($"{Program.GetClientCount()} clients connected");
+        }
+    }
+}
