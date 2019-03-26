b373c694e85004d1f784178ead6f421a378c2863
J Hershey <hersheyj32@gmail.com>
2019-03-26 15:37:14 -0700

    restore

--- JENPY/Storage/DataStore.cs
+++ JENPY/Storage/DataStore.cs
@@ -9,7 +9,21 @@ namespace JENPY.Storage
         public static IDictionary<String, string> DataValues { get; set;}
 
         static DataStore() {
-            DataValues = new Dictionary<string, string>();      
+            DataValues = new Dictionary<string, string>();
+
+            String BackupFileName = StorageConstants.FileName;
+            restore(DataValues, BackupFileName);      
+        }
+
+        private static void restore(IDictionary<string, string> dataValues, string backupFileName)
+        {
+            string[] lines = System.IO.File.ReadAllLines(backupFileName);
+            foreach (String keyVal in lines) {
+                string[] pair = keyVal.Split(',');
+                dataValues.Add(pair[0], pair[1]);
+            }
         }
     }
+
+
 }
---
+++ JENPY/Storage/StorageConstants.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace JENPY.Storage
+{
+    public class StorageConstants
+    {
+        public const string FileName = "/Users/jenny/Projects/JENPY/JENPY/Storage/mockDisk";
+    }
+}
