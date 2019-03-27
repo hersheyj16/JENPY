d878375b8444c9fc347441b3185a40ecd5d85ca9
J Hershey <hersheyj32@gmail.com>
2019-03-26 22:09:19 -0700

    background Timr worker

---
+++ JENPY/EmptyClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace JENPY
+{
+    public class EmptyClass
+    {
+        public EmptyClass()
+        {
+        }
+    }
+}
--- JENPY/Storage/DataStore.cs
+++ JENPY/Storage/DataStore.cs
@@ -1,28 +1,71 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
+using System.Threading;
 
 namespace JENPY.Storage
 {
 
     public class DataStore
     {
-        public static IDictionary<String, string> DataValues { get; set;}
+        public static IDictionary<String, string> DataValues { get; set; }
+        static Timer timer;
 
-        static DataStore() {
+        static DataStore()
+        {
             DataValues = new Dictionary<string, string>();
 
             String BackupFileName = StorageConstants.FileName;
-            restore(DataValues, BackupFileName);      
+            Restore(DataValues, BackupFileName);
+            PeriodicBackup();
         }
 
-        private static void restore(IDictionary<string, string> dataValues, string backupFileName)
+        private static void PeriodicBackup()
+        {
+            int MilliTenHours = 60 * 60 * 10 * 1000;
+            Console.WriteLine("debug - in periodic backup once for {0} miliseconds", MilliTenHours);
+            timer = new Timer(new TimerCallback(BackupHelper), null, 0, MilliTenHours);
+        }
+
+        private static void BackupHelper(Object obj)
+        {
+            String BackupFileName = StorageConstants.FileName;
+            BackupData(DataValues, BackupFileName);
+        }
+
+        private static void BackupData(IDictionary<string, string> dataValues, string backupFileName)
+        {
+            Console.WriteLine("running backup...");
+            //Adding a timestamp
+            StringBuilder sb = new StringBuilder();
+            sb.Append(backupFileName);
+            sb.Append("_");
+            sb.Append(DateTime.Now.ToString("M-d-yyyy"));
+            //sb.Append(Thread.CurrentThread.ManagedThreadId.ToString());
+            string OutputName = sb.ToString();
+
+
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(OutputName, false))
+            {
+                foreach (KeyValuePair<string, string> entry in dataValues)
+                {
+                    String s = String.Format("{0},{1}", entry.Key, entry.Value);
+                    file.WriteLine(s);
+                }
+            }
+
+        }
+
+        private static void Restore(IDictionary<string, string> dataValues, string backupFileName)
         {
             string[] lines = System.IO.File.ReadAllLines(backupFileName);
-            foreach (String keyVal in lines) {
+            foreach (String keyVal in lines)
+            {
                 string[] pair = keyVal.Split(',');
                 dataValues.Add(pair[0], pair[1]);
             }
         }
+
     }
 
 
