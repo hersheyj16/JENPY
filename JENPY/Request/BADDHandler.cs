6b7388582b041f3967d19b5ef7554165e551d44a
J Hershey <hersheyj32@gmail.com>
2019-03-28 14:15:11 -0700

    prior to testing

--- JENPY/Request/BADDHandler.cs
+++ JENPY/Request/BADDHandler.cs
@@ -1,11 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using JENPY.BlockChain;
+using JENPY.Utils;
+
 namespace JENPY.Request
 {
     public class BADDHandler : RequestHandler
     {
+
         public JenpyObject Handle(JenpyObject req)
         {
-            throw new NotImplementedException();
+            // serialize the string from req.ObjectData;
+            StringBuilder sb = new StringBuilder();
+
+            foreach (string key in req.ObjectData.Keys){
+                sb.Append(key);
+                sb.Append(",");
+                sb.Append(req.ObjectData[key]);
+            }
+
+            string input = sb.ToString();
+            Block b = new Block(input);
+            BlockStore.BlockChain.Add(b);
+            JenpyObject res = new JenpyObject(JenpyConstants.OK, req.ObjectData);
+            return res;
         }
     }
 }
--- JENPY/Request/BDESHandler.cs
+++ JENPY/Request/BDESHandler.cs
@@ -1,11 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
+using JENPY.BlockChain;
+using JENPY.Utils;
+
 namespace JENPY.Request
 {
     public class BDESHandler : RequestHandler
     {
         public JenpyObject Handle(JenpyObject req)
         {
-            throw new NotImplementedException();
+            IDictionary<string, string> data = new Dictionary<string, string>();
+            foreach (var block in BlockStore.BlockChain) {
+                data.Add(block.Hash, block.Data);
+            }
+
+            JenpyObject res = new JenpyObject(JenpyConstants.OK, data);
+            return res;
         }
     }
 }
