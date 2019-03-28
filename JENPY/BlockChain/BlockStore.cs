b059785c0ebfe186411969a4d0d65afd8e3ed3e5
J Hershey <hersheyj32@gmail.com>
2019-03-27 21:15:41 -0700

    a Proof of Concept blk chain

---
+++ JENPY/BlockChain/BlockStore.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JENPY.BlockChain
+{
+    public class BlockStore
+    {
+        public static IList<Block> BlockChain { get; set; }
+
+        public BlockStore()
+        {
+            BlockChain = new List<Block>();
+            //TODO think of a better way to stroe the genesis block
+            Block GenesisBlock = new Block();
+            GenesisBlock.Data = "none";
+            GenesisBlock.Hash = "none";
+            GenesisBlock.PrevHash = "none";
+
+            long Time = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+
+            GenesisBlock.Timestamp = Time;
+            BlockChain.Add(GenesisBlock);
+        }
+    }
+}
